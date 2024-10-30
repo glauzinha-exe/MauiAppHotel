1a48947b1641438eed29fb30e2a9680efe9ce293
glauzinha-exe <glauzinnha.exe23@gmail.com>
2024-10-30 14:12:53 -0300

    Conclusao page "sobre"

--- MauiAppHotel/Views/Sobre.xaml.cs
+++ MauiAppHotel/Views/Sobre.xaml.cs
@@ -6,4 +6,35 @@ public partial class Sobre : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+    {
+        var url = "https://github.com/glauzinha-exe";  
+        await Launcher.OpenAsync(url);     
+    }
+
+    private async void TapGestureRecognizer_Tapped_1(object sender, EventArgs e)
+    {
+        var url = "https://www.linkedin.com"; 
+        await Launcher.OpenAsync(url);       
+    }
+
+    private async void TapGestureRecognizer_Tapped_2(object sender, TappedEventArgs e)
+    {
+        var url = "https://github.com/glauzinha-exe/MauiAppHotel";
+        await Launcher.OpenAsync(url);
+    }
+
+    private void Button_Pressed(object sender, EventArgs e)
+    {
+        try
+        {
+            Navigation.PushAsync(new ContratacaoHospedagem());
+
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("ops", ex.Message, "ok");
+        }
+    }
 }
